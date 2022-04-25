e988cb35447e18cf81bee228081856f6b7cf7bc9
moween <moween1998@gmail.com>
2022-04-25 09:09:36 +0100

    Add testimonials avatar

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import subscriber1 from './nicolas-horn.jpg';
+import subscriber2  from './jake-nackos.jpg';
+import subscriber3 from './leilani-angel.jpg'
+
+export { subscriber1, subscriber2, subscriber3 };
